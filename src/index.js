55c10375edcb5bfeb99cf537d9b27c3eee1abcfe
angrytoro <angrytoro@gmail.com>
2017-12-03 11:01:44 +0800

    finish almost function

--- src/index.js
+++ src/index.js
@@ -1,41 +1,149 @@
 import xhook from 'xhook'
 
-let rules = {}
+let rules = []
 let basePathRegExp = null
+const PLACEHOLDER_REG = /:[^\/]+/g // 匹配restfull api中冒号开头的占位符
+const QUERY_REG = /\w+=[^&]+/g //
 
+/**
+ * 将restful路由生成可与对应路径匹配的正则表达式
+ * @param {*} path 
+ */
+function pathToRegexp(path) {
+  // 先去除掉以/结尾的接口,然后把占位符用正则替换，再加上结尾
+  let regStr = '^' + path.replace(/\/$/, '').replace(PLACEHOLDER_REG, '([^\\/]+?)') + '\\/?$'
+  return new RegExp(regStr, 'i')
+}
+
+/**
+ * 根据数据对象获取数据类型
+ * @param {*} obj 
+ */
+function getType(obj) {
+  return Object.prototype.toString.call(obj).slice(8, -1)
+}
+
+/**
+ * 
+ * @param {String} pathname 请求的路径
+ * @param {String} method 请求方法
+ */
 function findRule(pathname, method) {
-  for (let key in rules) {
-    let type = Object.prototype.toString.call(key).slice(8, -1)
-    if (type === 'String') {
-      
-    } else if (type === 'RegExp') {
-
+  return rules.find((rule) => {
+    if (rule.method.toUpperCase() !== method.toUpperCase()) {
+      return false
     }
+    let type = getType(rule.url)
+    if (type === 'String') {
+      if (rule.url.indexOf(':') > -1) {
+        rule.pathRegexp = rule.pathRegexp || pathToRegexp(rule.url)
+        if (rule.pathRegexp.test(pathname)) {
+          return true
+        }
+      } else if (rule.url === pathname) {
+        return true
+      }
+    } else if (type === 'RegExp' && rule.url.test(pathname)) {
+      return true
+    }
+    return false
+  })
+}
+
+/**
+ * 生成模拟数据
+ */
+function mock(request, rule , a) {
+  request.query = getQuery(a.search)
+  request.params = getParams(rule, a.pathname);
+  return rule.response(request)
+}
+
+/**
+ * 将get的查询条件由?key1=value1&key2=value2变成{key1: value1, key2: value2}
+ * @param {*} query 
+ */
+function getQuery(query = '') {
+  let queryMatches = query.match(QUERY_REG)
+  if (!queryMatches) {
+    return {}
+  } else {
+    let queryObj = {}
+    queryMatches.forEach((item) => {
+      let kv = item.split('='),
+        key = kv[0],
+        value = kv.splice(1).join('=')
+      queryObj[key] = value
+    })
+    return queryObj
   }
 }
 
+/**
+ * 对于restful接口 生成相应的参数
+ * @param {*} rule mock规则
+ * @param {*} pathname 用户请求路径
+ */
+function getParams(rule, pathname) {
+  if (rule.pathRegexp) {
+    let params = {},
+      urlMatches = rule.url.match(PLACEHOLDER_REG),
+      pathMatches = pathname.match(rule.pathRegexp);
+    urlMatches.forEach((value, index) => {
+      params[value.substr(1)] = pathMatches[index + 1]
+    })
+    return params
+  }
+  return {}
+}
+
 xhook.before((request, cb) => {
-  let url = new URL(request.url),
-    pathname = url.pathname.replace(basePathRegExp)
-  let rule = findRule(pathname, request.method)
-  // cb({
-  //   headers: request.headers,
-  //   status: 200,
-  //   statusText: 'OK',
-  //   text: '',
-  //   data: ''
-  // })
+  let a = document.createElement('a')
+  a.href = request.url
+  let pathname = a.pathname.replace(basePathRegExp),
+    rule = findRule(pathname, request.method)
+  if (rule) {
+    let mockData = JSON.stringify(mock(request, rule , a))
+    cb({
+      headers: request.headers,
+      status: 200,
+      statusText: 'OK',
+      text: mockData,
+      data: mockData
+    })
+  } else {
+    cb()
+  }
 })
 
-xhook.after((request, response) => {
-  console.log('after');
-})
+/**
+ * 将规则加入缓存
+ * @param {*} rule 
+ */
+function appendToRule(rule) {
+  let type = getType(rule.url)
+  if (type === 'String' && !/^\/[^\/]/.test(rule.url)) {
+    console.error(`${rule.url} must start with /, such as /${rule.url}`)
+  } else {
+    if (!rule.method) {
+      rule.method = 'GET'
+    }
+    rules.push(rule)
+  }
+}
 
-export default {
+module.exports = {
   setBasePath(basePath) {
     basePathRegExp = new RegExp(basePath)
   },
   mock(config) {
-    rules[config.url] = config
+    let type = getType(config)
+    if (type === 'Object') {
+      appendToRule(config)
+    } else if (type === 'Array') {
+      config.forEach((rule) => {
+        appendToRule(rule)
+      })
+    }
   }
 }
